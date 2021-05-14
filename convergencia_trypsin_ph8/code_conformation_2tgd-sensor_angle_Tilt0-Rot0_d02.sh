b5c4a6c47c9575c83ef276f9b6fbc8a84f6cd6f1
UrzuaSergio <sergio.urzua.6771@gmail.com>
2021-05-14 18:55:59 -0400

    archivos

---
+++ convergencia_trypsin_ph8/code_conformation_2tgd-sensor_angle_Tilt0-Rot0_d02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+###############################################################################################################################################################
+echo "Simulación que contempla las siguientes Orientaciones:"
+echo "Tilt begin: 0° - Tilt_end: 0° - Ntilt: 1 -- Rot begin: 0° - Rot end: 360° - Nrot: 1"
+echo "...................................................................................."
+echo "Proteina-Superficie: Peptido - Surf Cargada 1e-12 C/m^2."
+echo "Separación: 0.2 nm"
+python generador_config_file.py 2tgd_sensor 2tgd geometry/2tgd_d02 0 0 2
+
+echo "Se ha Creado Config File Auxiliar"
+echo "Comenzo Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando"
+echo "%%%%%%%% Simulando %%%%%%%%"
+
+python conformation_1hel.py 2tgd_sensor 2tgd_sensor/2tgd 2tgd_sensor/geometry/2tgd_d02 0 0 1 2 2tgd_d02-sensor_Tilt0-Rot0-2_EF1e-12
+
+echo "Termino Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando"
+##############################################################################################################################################################
