86cdb437c1a4cd29b9f0d5e69656117a678b0a0d
UrzuaSergio <sergio.urzua.6771@gmail.com>
2021-06-20 17:41:21 -0400

    1a0j trypsin-sensor files

---
+++ trypsin_pygbe_pH8/code_bash/code_conformation_1a0j-sensor_angle_0-20_EF5.5264e-5.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+ 
+#SBATCH -p gpuk 
+#SBATCH --gres=gpu:1 
+#SBATCH -J 1a0j-sensor_0-20_pH8_EF5.5264e-5 
+#SBATCH -o 1a0j-sensor_0-20_pH8_EF5.5264e-5-%j.out 
+#SBATCH -e 1a0j-sensor_0-20_pH8_EF5.5264e-5-%j.err 
+#SBATCH --mem=2G 
+#SBATCH --time=55:00:00 
+#SBATCH --mail-user=sergio.urzua.13@sansano.usm.cl 
+#SBATCH --mail-type=BEGIN,END,FAIL 
+ 
+use cuda10 
+use anaconda3 
+ 
+export PYTHONPATH=/user/s/surzua/PyCuda/pycuda/lib/python 
+cd ../../trypsin_pygbe_pH8/ 
+ 
+echo "Simulación que contempla las siguientes Orientaciones:" 
+echo "Tilt begin: 0° - Tilt_end: 20° - Ntilt: 6 -- Rot begin: 0° - Rot end: 360° - Nrot: 36" 
+echo "...................................................................................." 
+echo "Proteina-Superficie: Tripsina - Surf Cargada -0.05 C/m^2." 
+echo "Separación: 2 Angstrom" 
+ 
+python generador_config_file.py 1a0j_sensor 1a0j geometry/1a0j_d08_split 0 20 2 
+ 
+echo "Se ha Creado Config File Auxiliar" 
+echo "Comenzo Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
+echo "%%%%%%%% Simulando %%%%%%%%" 
+ 
+python conformation_1hel.py 1a0j_sensor 1a0j_sensor/1a0j 1a0j_sensor/geometry/1a0j_d08_split 0 20 6 2 1a0j-sensor_0-20-2_EF5.5264e-5 
+ 
+echo "Termino Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
---
+++ trypsin_pygbe_pH8/code_bash/code_conformation_1a0j-sensor_angle_144-160_EF-5.5264e-5.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+ 
+#SBATCH -p gpuk 
+#SBATCH --gres=gpu:1 
+#SBATCH -J 1a0j-sensor_144-160_pH8_EF-5.5264e-5 
+#SBATCH -o 1a0j-sensor_144-160_pH8_EF-5.5264e-5-%j.out 
+#SBATCH -e 1a0j-sensor_144-160_pH8_EF-5.5264e-5-%j.err 
+#SBATCH --mem=2G 
+#SBATCH --time=55:00:00 
+#SBATCH --mail-user=sergio.urzua.13@sansano.usm.cl 
+#SBATCH --mail-type=BEGIN,END,FAIL 
+ 
+use cuda10 
+use anaconda3 
+ 
+export PYTHONPATH=/user/s/surzua/PyCuda/pycuda/lib/python 
+cd ../../trypsin_pygbe_pH8/ 
+ 
+echo "Simulación que contempla las siguientes Orientaciones:" 
+echo "Tilt begin: 144° - Tilt_end: 160° - Ntilt: 5 -- Rot begin: 0° - Rot end: 360° - Nrot: 36" 
+echo "...................................................................................." 
+echo "Proteina-Superficie: Tripsina - Surf Cargada -0.05 C/m^2." 
+echo "Separación: 2 Angstrom" 
+ 
+python generador_config_file.py 1a0j_sensor 1a0j geometry/1a0j_d08_split 144 160 2 
+ 
+echo "Se ha Creado Config File Auxiliar" 
+echo "Comenzo Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
+echo "%%%%%%%% Simulando %%%%%%%%" 
+ 
+python conformation_1hel.py 1a0j_sensor 1a0j_sensor/1a0j 1a0j_sensor/geometry/1a0j_d08_split 144 160 5 2 1a0j-sensor_144-160-2_EF-5.5264e-5 
+ 
+echo "Termino Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
