aea7c4147c5f91bffdda5d5ddd9feab8c14600ab
UrzuaSergio <sergio.urzua.6771@gmail.com>
2021-06-30 20:49:53 -0400

    1smd amylase-sensor files

---
+++ amylase_pygbe_pH7/code_bash/code_conformation_1smd-sensor_angle_24-40_EF-5.5264e-5.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+ 
+#SBATCH -p gpuk 
+#SBATCH --gres=gpu:1 
+#SBATCH -J 1smd-sensor_24-40_pH7_EF-5.5264e-5 
+#SBATCH -o 1smd-sensor_24-40_pH7_EF-5.5264e-5-%j.out 
+#SBATCH -e 1smd-sensor_24-40_pH7_EF-5.5264e-5-%j.err 
+#SBATCH --mem=2G 
+#SBATCH --time=55:00:00 
+#SBATCH --mail-user=sergio.urzua.13@sansano.usm.cl 
+#SBATCH --mail-type=BEGIN,END,FAIL 
+ 
+use cuda10 
+use anaconda3 
+ 
+export PYTHONPATH=/user/s/surzua/PyCuda/pycuda/lib/python 
+cd ../../amylase_pygbe_pH7/ 
+ 
+echo "Simulación que contempla las siguientes Orientaciones:" 
+echo "Tilt begin: 24° - Tilt_end: 40° - Ntilt: 5 -- Rot begin: 0° - Rot end: 360° - Nrot: 36" 
+echo "...................................................................................." 
+echo "Proteina-Superficie: Amilasa - Surf Cargada -0.04 C/m^2." 
+echo "Separación: 2 Angstrom" 
+ 
+python generador_config_file.py 1smd_sensor 1smd_mod geometry/1smd_d08_split 24 40 2 
+ 
+echo "Se ha Creado Config File Auxiliar" 
+echo "Comenzo Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
+echo "%%%%%%%% Simulando %%%%%%%%" 
+ 
+python conformation_1hel.py 1smd_sensor 1smd_sensor/1smd_mod 1smd_sensor/geometry/1smd_d08_split 24 40 5 2 1smd-sensor_24-40-2_EF-5.5264e-5 
+ 
+echo "Termino Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
