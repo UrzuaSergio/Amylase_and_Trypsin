91418134ae32c94f2a724234e858cefe9f49f7c8
UrzuaSergio <sergio.urzua.6771@gmail.com>
2021-07-09 18:13:10 -0400

    beta-trypsin: 1fni-sensor files

---
+++ betatrypsin_pygbe_pH8/code_bash/code_conformation_1fni-sensor_angle_0-20_EF5.5264e-5.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+ 
+#SBATCH -p gpuk 
+#SBATCH --gres=gpu:1 
+#SBATCH -J 1fni-sensor_0-20_pH8_EF5.5264e-5 
+#SBATCH -o 1fni-sensor_0-20_pH8_EF5.5264e-5-%j.out 
+#SBATCH -e 1fni-sensor_0-20_pH8_EF5.5264e-5-%j.err 
+#SBATCH --mem=2G 
+#SBATCH --time=55:00:00 
+#SBATCH --mail-user=sergio.urzua.13@sansano.usm.cl 
+#SBATCH --mail-type=BEGIN,END,FAIL 
+ 
+use cuda10 
+use anaconda3 
+ 
+export PYTHONPATH=/user/s/surzua/PyCuda/pycuda/lib/python 
+cd ../../betatrypsin_pygbe_pH8/ 
+ 
+echo "Simulación que contempla las siguientes Orientaciones:" 
+echo "Tilt begin: 0° - Tilt_end: 20° - Ntilt: 6 -- Rot begin: 0° - Rot end: 360° - Nrot: 36" 
+echo "...................................................................................." 
+echo "Proteina-Superficie: Beta-Tripsina porcino - Surf Cargada -0.04 C/m^2." 
+echo "Separación: 2 Angstrom" 
+ 
+python generador_config_file.py 1fni_sensor 1fni geometry/1fni_d08_split 0 20 2 
+ 
+echo "Se ha Creado Config File Auxiliar" 
+echo "Comenzo Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
+echo "%%%%%%%% Simulando %%%%%%%%" 
+ 
+python conformation_1hel.py 1fni_sensor 1fni_sensor/1fni 1fni_sensor/geometry/1fni_d08_split 0 20 6 2 1fni-sensor_0-20-2_EF5.5264e-5 
+ 
+echo "Termino Ejecucion de PyGBe para Caso Proteina-Superficie Interactuando" 
